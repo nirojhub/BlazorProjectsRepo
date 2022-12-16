f3e67c6eb261fd312f7671b45774e1a0b32d7069
Niroj Kumar Sahoo <nirojkumarsahoo55@hotmail.co.uk>
2022-12-16 12:57:32 +0530

    4. Push new changes

---
+++ TelerikBlazorApp2/TelerikBlazorApp2/Controllers/CultureController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TelerikBlazorApp2.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class CultureController : Controller
+    {
+        public IActionResult SetCulture(string culture, string redirectUri)
+        {
+            if (culture != null)
+            {
+                HttpContext.Response.Cookies.Append(
+                    CookieRequestCultureProvider.DefaultCookieName,
+                    CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture, culture)));
+            }
+
+            return LocalRedirect(redirectUri);
+        }
+
+        public IActionResult ResetCulture(string redirectUri)
+        {
+            HttpContext.Response.Cookies.Delete(CookieRequestCultureProvider.DefaultCookieName);
+
+            return LocalRedirect(redirectUri);
+        }
+    }
+}
---
+++ TelerikBlazorApp2/TelerikBlazorApp2/Services/ResxLocalizer.cs
@@ -0,0 +1,22 @@
+﻿using Telerik.Blazor.Services;
+
+namespace TelerikBlazorApp2.Services
+{
+    public class ResxLocalizer : ITelerikStringLocalizer
+    {
+        // this is the indexer you must implement
+        public string this[string name]
+        {
+            get
+            {
+                return GetStringFromResource(name);
+            }
+        }
+
+        // sample implementation - uses .resx files in the ~/Resources folder names TelerikMessages.<culture-locale>.resx
+        public string GetStringFromResource(string key)
+        {
+            return Resources.TelerikMessages.ResourceManager.GetString(key, Resources.TelerikMessages.Culture);
+        }
+    }
+}
